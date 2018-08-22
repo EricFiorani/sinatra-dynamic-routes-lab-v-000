2a1bd335241012f3ee29f7d35aee21bdabf16e07
Eric Fiorani <efiorani305@gmail.com>
2018-08-22 14:15:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
     @name = params[:name]
-    r = @name
-    "#{r}".each(&:reverse!)
+    re = @name.each(&:reverse!)
+    "#{@name}"
   end
 end
